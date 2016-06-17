d6b3a8e56a6b9ee6d8693d432a2b6e2ed04db4b0
Jason Keenan <jason.keenan@raavin.com>
2016-06-17 23:01:46 +1000

    first commit to experimental branch

--- javascript.js
+++ javascript.js
@@ -1,49 +1,66 @@
 jQuery(document).ready(function($) {
-	console.log("test");
 
-	var calcKeys = [
+	var calcKeys = [ // ID names for keys
 	"percent", "divide",
 	"seven", "eight", "nine", "multiply",
 	"four", "five", "six", "minus",
 	"one", "two", "three", "plus",
 	"period", "zero"
-	]
-	var calcValues = [
+	];
+	var calcValues = [ // Values for eval()
 	"/10*", "/",
 	"7", "8", "9", "*",
 	"4", "5", "6", "-",
 	"1", "2", "3", "+",
 	".", "0"
-	] 
-	var current = []
-	var ans = 0
-	calcKeys.forEach( function(element, index) {
+	]; 
 
-		$( "#" + element.toString() ).click(function() {
+	var current = [0]; // Holds the array for eval
+	var display = [0]; // Holds Array for display
+	var ans = 0
+
+	calcKeys.forEach( function(element, index) { //iterate through ID names 
+
+		$( "#" + element.toString() ).click(function() { // create click for IDs
 			console.log(calcValues[index]);
-	  		//console.log(element);
-	  		current.push(calcValues[index]);
-	  		$(".result-output").html(current)
+
+			if ($.isNumeric(calcValues[index] && $.isNumeric(current[current.length]))){
+					current.push(calcValues[index]);
+					display.push(calcValues[index]);
+					$(".result-output").html(display);
+			} else {
+					display = [0];
+			}
+	  		//$.isNumeric(calcValues[index]) ? current.push(calcValues[index]) : ; //if clicked, push value to array
+	  		//$.isNumeric(calcValues[index]) ? display.push(calcValues[index]) : display = []; // if entry is a number push value display array
+	  		//$.isNumeric(calcValues[index]) ? $(".result-output").html(display) : ""; // if entry is a number display it, else clear display
+	  		console.log(current + ":curr");
+			console.log(display + ":disp");
 		});
 	});
 
 	$( "#ac" ).click(function() {
-			
-			
-			current = [];
+			current = [];						// clear array
+			display = [];						// clear array
 			console.log(current + ": value");
-	  		$(".result-output").html(0);
+			console.log(current + ":curr");
+			console.log(display + ":disp");
+	  		$(".result-output").html(0);		// Set display to zero
 	});
 
-	$( "#ce" ).click(function() {
-	  		current.pop();
-	  		$(".result-output").html(current);
+	$( "#ce" ).click(function() {				
+	  		current.pop();						// pop last character off the end of the array when ce is pressed
+	  		$(".result-output").html(0);		// set output to zero
 	});
 
 	$( "#equal" ).click(function() {
 		try{
-			ans = (Math.floor((eval(current.join(""))) * Math.pow(10, 13))) / Math.pow(10, 13);
-	  		$(".result-output").html((Math.floor((eval(current.join(""))) * Math.pow(10, 13))) / Math.pow(10, 13));
+			ans = floatCompensate(current.join(""));						//					
+	  		$(".result-output").html(floatCompensate(current.join("")));
+	  		display = [];													// clear display array
+	  		current = [ans];												// put previous result in current array 
+	  		console.log(current + ":curr");
+			console.log(display + ":disp");
 		}
 		catch(err){
 
@@ -57,6 +74,7 @@ jQuery(document).ready(function($) {
 	});
 	
 	function floatCompensate(num){
-		(Math.floor((eval(num.join(""))) * Math.pow(10, 13))) / Math.pow(10, 13)
+		return (Math.floor((eval(num)) * Math.pow(10, 13))) / Math.pow(10, 13)
 	};
+
 });
